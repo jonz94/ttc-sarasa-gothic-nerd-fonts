3c6f441758e2e38f952245a5bbd48eeefe7cee64
jonz94 <jody16888@gmail.com>
2023-10-08 17:10:34 +0800

    🔧 config: migrate `preitter` settings to v3

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+
+  plugins: ['prettier-plugin-packagejson'],
+}
+
+export default config
